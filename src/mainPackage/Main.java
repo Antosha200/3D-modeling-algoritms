ee46d3fd6e6d8e5856d6f6dbdc62707d01052e72
Anton <anton2-2000@mail.ru>
2021-10-05 16:09:46 +0300

    1

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -8,7 +8,7 @@ public class Main {
     private static Figure figure;
 
     public static void main(String[] args) {
-        figure = new MyFigure(200, 200, 200, 100, 100);
+        figure = new MyFigure(300, 200, 100, 50, 100);
         new MainFrame("3D");
     }
 
